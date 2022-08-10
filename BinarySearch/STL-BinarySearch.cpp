c07ee4765b8f94b5ab09328ae101c1c2226e0030
ManishV <manish.vohra@gmail.com>
2022-08-10 21:02:53 +0200

    Added .gitignore file

---
+++ BinarySearch/STL-BinarySearch.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+
+using namespace std;
+
+class Solution
+{
+
+public:
+
+    bool search(vector<int> v, int t)
+    {
+        sort(v.begin(), v.end());
+        return binary_search(v.begin(), v.end(),t);
+    }
+
+};
+
+int main(){
+
+    vector<int> v = {2,5,6,7,3,1,0};
+    Solution s;
+    cout<<"Target number is present :"<<s.search(v,5);
+    
+    return1;
+}
