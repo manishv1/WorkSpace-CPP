21ed10f98efd5433123ae8675b4b46de61e63b7f
Manish Vohra <manish.vohra@gmail.com>
2022-09-03 16:43:15 +0200

    Test

--- Array/Target-Sum.cpp
+++ Array/Target-Sum.cpp
@@ -15,8 +15,8 @@ public:
         std::vector<int>::iterator it;
         
         
+        // Commetns needs to be added 
 
-        
         for( int i = 0 ; i < nums.size(); i++ ){
 
                 k = target - nums[i];
