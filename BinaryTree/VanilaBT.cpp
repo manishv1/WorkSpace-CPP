cc33a4d072b716d29153a80e8318ca95718083d8
ManishV <manish.vohra@gmail.com>
2022-09-10 17:32:02 +0200

    Added snipped for class node

---
+++ BinaryTree/VanilaBT.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+
+
+
+class node
+{
+    public:
+        int data;
+        node *lc;
+        node *rc;
+   
+        node(int data){
+            this->data = data;
+            lc = NULL;
+            rc = NULL;
+        }    
+};
+
+
+
+ node *insert(node *root,int data )
+ {      
+      if(root == NULL){
+        return new node(data);
+      }
+
+      if( data < root->data){
+        root->lc = insert(root->lc,data);
+      }
+      else{
+        root->rc = insert(root->rc, data);
+      }
+
+      return root;
+ }
+
+void inorder(node *root){
+
+      if(root == NULL ){
+
+            return;
+      }
+      inorder(root->lc);
+      std::cout << root->data << std::endl;
+      inorder(root->rc);
+}
+
+
+
+ 
+ 
+int main()
+{
+   node *root = NULL;
+   NULL;
+   vector<int> v = {100,60,200,40,75,110,250};
+
+   for (int i = 0; i < v.size(); i++)
+   {    
+       root =  insert(root,v[i]);
+   }   
+    inorder(root);
+    return 0;
+}
