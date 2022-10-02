a1e3436b28a61c49ca4a418a3bf3ef0c1b13202d
ManishV <manish.vohra@gmail.com>
2022-10-02 17:56:29 +0200

    .

---
+++ Array/TargetSum/StoreElementIndex.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+vector<int> v = { 11,2,13,4,5,16,7,18,9};
+
+map<int,int> m;
+
+ 
+int main()
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        m[v[i]] = i;   
+    }
+
+
+   //show all elements to left of 5
+
+   int idx = m[5];
+
+   for (int i = 0; i < idx; i++)
+   {
+        std::cout << v[i]<<", " << std::endl;
+    
+   }
+   
+    return 0;
+}
+
+
