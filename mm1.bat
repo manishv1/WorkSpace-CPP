b95ef3123123a9f3e592ae146e0a3734ca98d584
Manish  Vohra <manish.vohra@gmail.com>
2023-10-14 13:21:26 +0530

    Update mm1.bat
    Updated for batter work

--- mm1.bat
+++ mm1.bat
@@ -1,4 +1,29 @@
 @echo off
-echo Compiling file :%1
-g++ -o a.exe %1
+
+REM Get the file name
+set file_name=%1
+
+REM Get the input variable
+set input_variable=%1
+
+REM Remove the path from the input variable
+set inputFileName=%~n1
+echo %inputFileName%
+
+REM Append .exe to the file name
+set outfile=%inputFileName%.exe
+
+echo %outfile%
+g++ -o %outfile% %1
+
+REM Check the status of the last command
+IF %ERRORLEVEL% NEQ 0 (
+	echo "Compilation Failed .........."
+    EXIT /B 1
+)ELSE (
+
+	
+	echo  "Compilation Successful......."
+	)
+
 
